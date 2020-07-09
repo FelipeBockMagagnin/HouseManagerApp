5d12ca9d8e24cedaec91459a2c25f95cc42c85b6
Felpi <felipebmagagnin@gmail.com>
2020-07-09 13:46:38 -0300

    feat: add button on todolist page

--- src/Pages/ToDoList/todolist.js
+++ src/Pages/ToDoList/todolist.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, Button, View } from 'react-native'
 import Header from '../../Components/Header/header'
 
 export default function Todolist ({ navigation }) {
+  const [items, setItems] = useState([])
+
   return (
     <View>
       <Header title='Todolist' navigation={navigation}></Header>
-      <Text>Todolist</Text>
+
+      <View style={{ display: 'flex', paddingHorizontal: 10 }}>
+        <View>
+          {items.map(item => {
+            return (
+              <View key={item.id} style={{ margin: 10, borderWidth: 2, padding: 10 }}>
+                <Text>{item.name}</Text>
+                <Text>{item.creationDate}</Text>
+              </View>
+            )
+          })}
+        </View>
+
+        <Button title='add' onPress={() => { setItems([...items, { id: 1, name: 'tarefa', creationDate: new Date().toLocaleString() }]) }}></Button>
+        <Button title='clear' onPress={() => { setItems([]) }}></Button>
+      </View>
     </View>
   )
 }
