1d1d088a2fd6460c6f63ae21648999e7a8ab3016
Felpi <felipebmagagnin@gmail.com>
2020-09-08 22:37:24 -0300

    feat: improve home page looking

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,13 +1,56 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Text, View, ScrollView } from 'react-native'
 import Header from '../../components/Header/header'
+import { GetTodoList } from '../../services/todolistService'
+import { Card, Title, Paragraph, Button, Avatar } from 'react-native-paper'
 
 export default function Home ({ navigation }) {
+  const [todo, setTodo] = useState([])
+
+  useEffect(() => {
+    GetTodoList().then(response => {
+      setTodo(response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+  const LeftContent = props => <Avatar.Icon {...props} icon="format-list-bulleted-square" />
+
   return (
     <View>
-      <Header title='Home' navigation={navigation}></Header>
+      <Header title='Tarefas' navigation={navigation}></Header>
 
-      <Text>Description of house status</Text>
+      <ScrollView>
+        <Card style={{ padding: 10, margin: 10 }} >
+          <Card.Title title="Tarefas" left={LeftContent} />
+          <Card.Content>
+            <Paragraph>Numero de tarefas a fazer: {todo.length}</Paragraph>
+          </Card.Content>
+        </Card>
+
+        <Card style={{ padding: 10, margin: 10 }} >
+          <Card.Title title="Compras" left={LeftContent} />
+          <Card.Content>
+            <Paragraph>a fazer</Paragraph>
+          </Card.Content>
+        </Card>
+
+        <Card style={{ padding: 10, margin: 10 }} >
+          <Card.Title title="Contas" left={LeftContent} />
+          <Card.Content>
+            <Paragraph>a fazer</Paragraph>
+          </Card.Content>
+        </Card>
+
+        <Card style={{ padding: 10, margin: 10, marginBottom: 100 }} >
+          <Card.Title title="Reformas" left={LeftContent} />
+          <Card.Content>
+            <Paragraph>a fazer</Paragraph>
+          </Card.Content>
+        </Card>
+      </ScrollView>
     </View>
+
   )
 }
