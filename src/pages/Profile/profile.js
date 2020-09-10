8b778e041155f2ed88eaee464411eb5c0a180441
Felpi <felipebmagagnin@gmail.com>
2020-09-10 19:38:34 -0300

    feat: logout button

--- src/pages/Profile/profile.js
+++ src/pages/Profile/profile.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import Header from '../../components/Header/header'
+import { Button } from 'react-native-paper'
 
 export default function Profile ({ navigation }) {
   return (
     <View>
       <Header title='Profile' navigation={navigation}></Header>
       <Text>Profile</Text>
+      <Button onPress={() => { navigation.navigate('Login') }}>Sair</Button>
+
     </View>
   )
 }
