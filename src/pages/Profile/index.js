68f70e58780655ea3f4e0ba3aac80f3489b7dac1
Felpi <felipebmagagnin@gmail.com>
2020-09-10 21:37:59 -0300

    refactor: improve profile page code

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import Header from '../../components/Header/header'
+import Header from '../../components/Header'
 import { Button } from 'react-native-paper'
 
 export default function Profile ({ navigation }) {
@@ -9,7 +9,6 @@ export default function Profile ({ navigation }) {
       <Header title='Profile' navigation={navigation}></Header>
       <Text>Profile</Text>
       <Button onPress={() => { navigation.navigate('Login') }}>Sair</Button>
-
     </View>
   )
 }
