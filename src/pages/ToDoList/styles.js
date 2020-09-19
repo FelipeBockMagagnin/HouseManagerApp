a9071c0e2426ee4184d79f1d67297c4c879ce03a
Felpi <felipebmagagnin@gmail.com>
2020-09-19 17:54:09 -0300

    refactor: remove unused css

--- src/pages/ToDoList/styles.js
+++ src/pages/ToDoList/styles.js
@@ -1,59 +1,11 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#333',
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
-  },
-  openButton: {
-    backgroundColor: '#333',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    marginTop: 15
-  },
-  input: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    height: 40,
-    width: 200,
-    borderColor: '#333',
-    borderWidth: 1
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  modalText: {
-    marginTop: 15,
-    textAlign: 'center'
-  },
   todoContainer: {
-    margin: 5,
-    borderWidth: 2,
-    padding: 5,
+    paddingVertical: 50,
+    paddingHorizontal: 20,
     borderRadius: 10,
-    display: 'flex',
-    flexDirection: 'row',
+    margin: 10,
     backgroundColor: 'white'
   },
   addButton: {
