721bcf3ae1938bf2be799dcb9154e91e5d3cf114
Felpi <felipebmagagnin@gmail.com>
2020-11-19 10:47:26 -0300

    chore: add gogle login button and small improvments in login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { Text, Button, View } from 'react-native'
+import { Text, Button, View, Image } from 'react-native'
 import * as Google from 'expo-google-app-auth'
+import { SocialIcon } from 'react-native-elements'
 
 export default function Login ({ navigation }) {
-  const [user, setUser] = useState()
+  const [user, setUser] = useState(null)
 
-  useEffect(() => {
+  function signOutAsync () {
+    setUser(null)
+  }
+
+  function signInAsync () {
     Google.logInAsync({
       androidClientId: '496207766849-c39hv9vl15nh0o85k243e5cggft3sgm0.apps.googleusercontent.com',
       scopes: ['profile', 'email']
@@ -15,20 +20,14 @@ export default function Login ({ navigation }) {
       console.log(user)
       console.log('chegou aqui')
       if (type === 'success') {
+        setUser(user)
+
         // Then you can use the Google REST API
         console.log('sucess')
       }
     }).catch(err => {
       console.log(err)
     })
-  }, [])
-
-  function signOutAsync () {
-
-  }
-
-  function signInAsync () {
-
   }
 
   function signIn () {
@@ -43,8 +42,24 @@ export default function Login ({ navigation }) {
 
   return (
     <View>
-      <Text>Login com Google</Text>
-      <Button title='Logar Com Google' onPress={ () => signIn() }/>
+      {user
+        ? <View>
+          <Text>Usuario Logado: </Text>
+          <Text>Email: { user.email }</Text>
+          <Text>Name: { user.givenName } {user.familyName}</Text>
+          <Image style={{
+            width: 50,
+            height: 50
+          }} source={{
+            uri: user.photoUrl
+          }} />
+        </View> : null}
+      <SocialIcon
+        title={user ? 'Deslogar' : 'Login com Google'}
+        button={true}
+        type={'google'}
+        onPress={() => signIn()}
+      />
     </View>
   )
 }
